0735d0c1c452506528b3375939a68e2de53819fd
alexm323 <alejandromartinezjr42@gmail.com>
2021-01-19 10:29:36 -0800

    added refactor of colored circles app

--- dice-design/src/App.js
+++ dice-design/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Dice from './Dice'
-
+import ColoredCircles from './ColoredCircles1/ColoredCircles'
 function App() {
   return (
     <div className="App">
-      <Dice numDice={4} maxVal={6} />
-      <Dice title='Roll Me' />
+      {/* <Dice numDice={4} maxVal={6} />
+      <Dice title='Roll Me' /> */}
+      <ColoredCircles />
     </div>
   );
 }
